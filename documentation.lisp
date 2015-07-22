e4e46d49784da6a308e67fd9582760a930e3a744
Shinmera <shinmera@tymoon.eu>
2015-07-22 16:32:30 +0200

    Documentation

--- documentation.lisp
+++ documentation.lisp
@@ -6,7 +6,19 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 
 (in-package #:org.shirakumo.simple-tasks)
 
+(defun checkdocs (&optional (package *package*))
+  "Check that all functions, classes, and variables have docstrings."
+  (do-symbols (symb package)
+    (when (eql (symbol-package symb) package)
+      (when (and (fboundp symb) (not (documentation symb 'function)))
+        (warn "No documentation for function ~s." symb))
+      (when (and (boundp symb) (not (documentation symb 'variable)))
+        (warn "No documentation for variable ~s." symb))
+      (when (and (find-class symb NIL) (not (documentation symb 'type)))
+        (warn "No documentation for class ~s." symb)))))
+
 (defmacro setdocs (&body pairs)
+  "Easily set the documentation."
   `(progn
      ,@(loop for (var doc) in pairs
              collect (destructuring-bind (var &optional (type 'function))
@@ -40,26 +52,17 @@ See RUN-TASK")
   
   (run-task
    "Run the given task object directly.")
+
+  (interrupt-task
+   "Interrupt the TASK to stop it from execution on RUNNER.
+
+If the task is currently running, it is forcibly aborted. In either case,
+the task's status is changed to :COMPLETED and it will not execute further.
+On systems without thread support this does nothing.")
  
   ((runner type)
    "Basic runner. Runs task as soon as scheduled.")
-  
-  
-  (status
-   "Current status indicator of the runner/task.
-The following statuses are defined:
 
-:CREATED     Object has been initialized, but not started.
-:RUNNING     Object is currently executing.
-:STOPPING    Runner is in the process of being stopped.
-:STOPPED     Runner has been stopped.
-:COMPLETED   Task successfully completed execution.
-:ERRORED     Task ended execution with an error.")
- 
-  ((queued-runner type)
-   "Queued runner. Runs tasks in a thread if threading is available.
-Noe that START-RUNNER for this runner will block the current thread.")
-  
   (queue
    "The current task queue of the runner.
 
@@ -71,13 +74,33 @@ when you look at the queue at any particular moment, tasks that are not
 in it might not have run yet.")
   
   (lock
-   "The lock used to synchronise operations with the object.")
+   "The lock used to synchronise operations with the object.
+
+See QUEUED-RUNNER
+See BLOCKING-CALL-TASK")
   
   (cvar
    "The condition variable used to exchange signals.
 
 See QUEUED-RUNNER
 See BLOCKING-CALL-TASK")
+
+  (thread
+   "Returns the current thread associated with the queued-runner if any.
+
+See QUEUED-RUNNER")
+ 
+  ((queued-runner type)
+   "Queued runner. Runs tasks in a thread if threading is available.
+Noe that START-RUNNER for this runner will block the current thread.")
+
+  ((*current-queue* variable)
+   "Bound to either NIL or the current queue being processed by the queued-runner.
+
+See QUEUED-RUNNER")
+
+  ((*current-task* variable)
+   "Bound to either NIL or the current task being processed by the queued-runner.")
   
   (make-runner-thread
    "Make a thread to call START-RUNNER on RUNNER in.
@@ -87,13 +110,49 @@ On platforms without, this simply calls START-RUNNER and returns NIL.
 
 See START-RUNNER"))
 
+;; status.lisp
+(setdocs
+  ((+status-started+ variable)
+   "Constant matching any started (not necessarily running) status.
+
+:CREATED     Object has been initialized, but not started.
+:RUNNING     Object is currently executing.")
+
+  ((+status-running+ variable)
+   "Constant matching a running status.
+
+:RUNNING     Object is currently executing.")
+
+  ((+status-ended+ variable)
+   "Constant matching any ended (not necessarily successful) status.
+
+:STOPPING    Object is in the process of being stopped.
+:STOPPED     object has been stopped.
+:COMPLETED   Object successfully completed execution.
+:ERRORED     Object ended execution with an error.")
+  
+  (status
+   "Current status indicator of the status-object.")
+
+  (status=
+   "Compare two statuses with each other (commutative).")
+
+  ((status-object type)
+   "A class that has a status.")
+
+  (status-list-p
+   "Returns true if every element in the list is a symbol and the list thus qualifies as a STATUS.")
+
+  ((status type)
+   "A status can be a SYMBOL, a STATUS-OBJECT, or a LIST composed of SYMBOLs."))
+
 ;; task.lisp
 (setdocs
-  ((task-condition type)
-   "Condition superclass for task operation related conditions.")
-
   (task
    "The task related to the condition.")
+  
+  ((task-condition type)
+   "Condition superclass for task operation related conditions.")
 
   ((task-already-scheduled type)
    "Condition signalled when attempting to reschedule an already scheduled task.")
@@ -108,6 +167,13 @@ See START-RUNNER"))
    "The runner the task is scheduled on.
 
 See TASK")
+
+  (await
+   "Wait for the TASK to match a certain STATUS.
+
+On systems without thread support this does nothing.
+
+See STATUS=")
 
   (error-environment
    "An environment object that is stored in case the task fails to run properly.
@@ -126,14 +192,43 @@ See CALL-TASK")
    "Returns the values that the call returned.
 
 See CALL-TASK")
+
+  ((notifying-task type)
+   "A task that will notify a condition upon completion.
+
+This is particularly useful in conjunction with AWAIT.")
+
+  ((blocking-task type)
+   "A task that will block after being scheduled until it is done or interrupted.
+
+When SCHEDULE-TASK is called on this, it establishes two restarts:
+ABORT    to forcibly abort (interrupt) the task.
+UNBLOCK  to resume execution in the current thread and leave the task running.
+
+If a restart or similar exit functionality is invoked that leaves the scope of
+SCHEDULE-TASK, the task is interrupted.
+
+Note that the restarts are not useful on systems without thread support. In such
+a case the task will be interrupted either way, due to the very nature of running
+in the current thread.
+
+See AWAIT
+See INTERRUPT-TASK
+See NOTIFYING-TASK")
   
   ((blocking-call-task type)
    "Task class to perform a function call once run.
-Blocks the scheduling thread until it is done.")
+Blocks the scheduling thread until it is done.
+
+See CALL-TASK
+See BLOCKING-TASK")
   
   (call-as-task
    "Call function within a task, usually a BLOCKING-CALL-TASK.
-Returns the values that have been saved in the task after SCHEDULE-TASK returns.
+Depending on the task's STATUS after SCHEDULE-TASK returns, the following happens.
+  :COMPLETED  The task's return values are returned.
+  :ERRORED    A condition of type TASK-ERRORED is signalled.
+  T           The task is returned.
 
 See SCHEDULE-TASK
 See BLOCKING-CALL-TASK")
@@ -145,15 +240,21 @@ See CALL-AS-TASK")
   
   ((callback-task type)
    "Task class to perform a function call once run and call a callback upon completion.
-The callback function is called with each return value as an argument.
-Note that the callback function is called within the runner environment
-which may be different from the scheduler environment.")
+
+If the task completes successfully, the callback function is called with each return
+value as an argument. Note that the callback function is called within the runner
+environment which may be different from the scheduler environment.
+
+See CALL-TASK")
   
   (callback
    "The function to call upon completion of the task.
 
 See CALLBACK-TASK"))
-;; task.lisp
+;; toolkit.lisp
 (setdocs
   ((no-threading-stump type)
-   "Stump class to stand in place of a value on systems without threading support."))
+   "Stump class to stand in place of a value on systems without threading support.")
+
+  ((+no-threading-stump+ variable)
+   "Constant to hold an instance of NO-THREADING-STUMP."))
